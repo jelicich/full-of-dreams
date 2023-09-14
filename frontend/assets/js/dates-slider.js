213e5a57c3eb766603b81d43f88c95cb3253fa0e
Esteban Jelicich <jelicich.e@gmail.com>
2023-09-14 23:52:43 +0200

    Add Availability section

---
+++ frontend/assets/js/dates-slider.js
@@ -0,0 +1,26 @@
+(function($) {
+
+  const SLIDER_SELECTOR = '#dates-slider';
+  const SLIDE_SELECTOR = '.keen-slider__slide';
+  const ACTIVE_CLASS = 'is-active';
+
+  new KeenSlider(SLIDER_SELECTOR, {
+    mode: 'snap',
+    range: { align: false },
+    rubberband: false,
+    slides: {
+      origin: 'auto',
+      perView: 'auto',
+      spacing: '8'
+    },
+  })
+
+  $(SLIDE_SELECTOR).click(function() {
+    $(SLIDE_SELECTOR).removeClass(ACTIVE_CLASS);
+    $(this).addClass(ACTIVE_CLASS);
+    
+    // TODO: handle date selection
+    console.log($(this).data('date'));
+  })
+
+})(jQuery)
